0b3dd99aeff0349dce6220f11041ce2434622f5d
Solka99 <alicjas508@gmail.com>
2024-12-03 15:55:40 +0100

    Add files

---
+++ game/agent.py
@@ -0,0 +1,10 @@
+class Agent:
+    def __init__(self, id, x, y, direction, age, health_state):
+        self.id = id,
+        self.x = x,
+        self.y = y,
+        self.position = (x, y)
+        self.direction = direction
+        self.age = age,
+        self.health_state = health_state
+
---
+++ game/main.py
@@ -0,0 +1,7 @@
+import pygame
+import numpy as np
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
