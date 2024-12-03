0e04654cdace059e34515291c910ba1f51b7e961
Solka99 <alicjas508@gmail.com>
2024-12-03 17:38:40 +0100

    Agents added to main

--- game/agent.py
+++ game/agent.py
@@ -1,10 +1,10 @@
 class Agent:
-    def __init__(self, id, x, y, direction, age, health_state):
-        self.id = id,
-        self.x = x,
-        self.y = y,
+    def __init__(self, id, x, y, age, health_state):
+        self.id = id
+        self.x = x
+        self.y = y
         self.position = (x, y)
-        self.direction = direction
-        self.age = age,
+        # self.direction = direction
+        self.age = age
         self.health_state = health_state
 
--- game/main.py
+++ game/main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import random
+from game.agent import Agent
 
 pygame.init()
 
@@ -16,11 +17,17 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption('Pandemic Spread Simulation')
 
+
+
 random.seed(10)
 for i in range(100):
     x = random.randint(0, SCREEN_WIDTH)
     y = random.randint(0, SCREEN_HEIGHT)
-    pygame.draw.circle(screen, GREEN, (x, y), 4)
+    age = random.randint(1, 100)
+    agent = Agent(i,x,y, age, 'S')
+    pygame.draw.circle(screen, GREEN, (agent.x, agent.y), 4)
+
+pygame.display.update()
 
 
 while True:
