681bb79c7b92b345d5a7cf476d2d4194eefe2b10
Solka99 <alicjas508@gmail.com>
2024-12-03 20:56:49 +0100

    Deleted redundant code

--- game/agent.py
+++ game/agent.py
@@ -12,22 +12,13 @@ class Agent:
         self.y = y
         self.position = pygame.math.Vector2(x, y)
         self.direction = pygame.math.Vector2(random.uniform(-1, 1), random.uniform(-1, 1)).normalize()
-        self.speed = 3
+        self.speed = 2
         self.age = age
         self.health_state = health_state
 
-    def choose_random_direction(self):
-        random_direction = random.randint(0, 359)
-        return random_direction
-
-    def calculate_movement_vector(self):
-        direction = self.choose_random_direction()
-        vector = pygame.math.Vector2(self.x, self.y).rotate(direction)
-        print('Vector: ',vector)
-        return vector
-
     def move(self):
         self.position += self.direction * self.speed
+
         if self.position.x <= 0:
             self.direction.x *= -1
             self.position.x = 0
