9bed929075bcef69bcdf9a5dd1682737c4e3a4d5
Solka99 <alicjas508@gmail.com>
2024-12-27 12:29:26 +0100

    Plot minor fix

--- game/plot.py
+++ game/plot.py
@@ -35,7 +35,7 @@ class PlotManager:
             self.recovered_counts[-1],
             self.dead_counts[-1],
         ]
-        plt.ylim(0, 100)
+        plt.ylim(0, 100) # można tu dać zmienną ilości agentów
 
         plt.bar(categories, values, color=["green", "yellow", "red", "blue", "gray"])
         plt.xlabel("Health States")
@@ -48,10 +48,10 @@ class PlotManager:
         def update(frame):
             self.update_data(frame,agents)  # Przekazywanie agents do update_data
 
-        ani = FuncAnimation(plt.gcf(), update, interval=500)
+        ani = FuncAnimation(plt.gcf(), update,frames=None, interval=500)
         plt.show()
 
     def run_in_thread(self, agents):
         """Uruchamia wykres w osobnym wątku."""
-        plot_thread = threading.Thread(target=self.animation, args=(agents,), daemon=True)
+        plot_thread = threading.Thread(target=self.animation, args=(agents,), daemon=True) # wyłączy się jak wyłączy się główny program
         plot_thread.start()
